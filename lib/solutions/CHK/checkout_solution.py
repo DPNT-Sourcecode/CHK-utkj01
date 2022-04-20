9bd2b3a947e4aee113d169f99b1635e79afb541f
sbx_user1051 <sbx_user1051@169.254.17.29>
2022-04-20 07:29:57 +0000

    Wed Apr 20 07:29:57 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,21 @@
+from enum import Enum, auto
+
+
+class Product(Enum):
+    A = 50
+    B = 30
+    C = 20
+    D = 15
+
+    def __init__(self, price: int):
+        self._value_ = auto()
+        self.price = price
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
     raise NotImplementedError()
+
 
 
