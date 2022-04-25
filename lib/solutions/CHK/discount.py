98dfbad613ec199b1339412f2a285a06bbad7d1d
sbx_user1051 <sbx_user1051@169.254.173.133>
2022-04-25 08:14:42 +0000

    Mon Apr 25 08:14:42 UTC 2022

--- lib/solutions/CHK/discount.py
+++ lib/solutions/CHK/discount.py
@@ -26,21 +26,22 @@ class GetFree(Discount):
                 if free_product is not product:
                     products[free_product] -= 1
                 products[product] -= self.offer["count"]
+        return discount
 
 
 class MultiPrice(Discount):
     def __init__(self, offer: List[dict]) -> None:
         self.offer = offer
+
     def apply(self, products: Dict[Product, int]) -> int:
         discount = 0
-        for offer in offers:
-            product = offer['product']
-            if isinstance(product, Product):
-                if 'price' in offer and product in basket:
-                    while basket[product] >= offer["count"]:
-                        discount += product.price * offer["count"] - offer['price']
-                        basket[product] -= offer["count"]
-        return discount, basket
+        product = self.offer['product']
+        if isinstance(product, Product):
+            if 'price' in self.offer and product in products:
+                while products[product] >= self.offer["count"]:
+                    discount += product.price * self.offer["count"] - self.offer['price']
+                    products[product] -= self.offer["count"]
+        return discount
 
 
 class GroupPrice(Discount):
@@ -51,18 +52,17 @@ class GroupPrice(Discount):
         discount = 0
         total = 0
         applied = 0
-        for offer in offers:
-            product_list = offer['product']
-            if isinstance(product_list, list):
-                for product in sort_pricier_first(product_list):
-                    if product in basket:
-                        while basket[product] > 0:
-                            total += product.price
-                            applied += 1
-                            basket[product] -= 1
-                if applied >= offer['count']:
-                    discount += total - offer['price'] * applied / offer['count']
-        return discount, basket
+        product = self.offer['product']
+        for product in sort_pricier_first(product):
+            if product in products:
+                while products[product] > 0:
+                    total += product.price
+                    applied += 1
+                    products[product] -= 1
+        if applied >= self.offer['count']:
+            discount += total - self.offer['price'] * applied / self.offer['count']
+        return discount
+
 
 
 
